a8baa8d4a852442497154df84f537b04dfb3afa1
bz421 <bozhan.harry@gmail.com>
2024-05-20 13:38:25 -0400

    more server code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import AuthContext from './Contexts/AuthContext'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
+  const [user, setUser]  = useState(null)
+  const [isInitiated, setIsInitiated] = useState(false)
+
+  const handleLogout = () => {
+    setUser(null)
+    localStorage.setItem('token', null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isInitiated && (
+        <AuthContext.Provider value={{user, setUser, handleLogout}}>
+          
+        </AuthContext.Provider>
+      )}
     </div>
   );
 }
