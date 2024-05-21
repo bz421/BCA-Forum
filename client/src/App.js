171791fa8afc2d19421e2f47c56a5195363bbf20
bz421 <bozhan.harry@gmail.com>
2024-05-21 14:41:47 -0400

    Created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
+import { Fragment } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import AuthContext from './Contexts/AuthContext'
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 
+import Login from './Pages/Auth/Login'
+
 function App() {
-  const [user, setUser]  = useState(null)
+  const [user, setUser] = useState(null)
   const [isInitiated, setIsInitiated] = useState(false)
 
   const init = () => {
     setIsInitiated(true)
   }
 
-  useEffect( () => {
+  useEffect(() => {
     init()
   }, [])
 
@@ -25,10 +28,14 @@ function App() {
   return (
     <div>
       {isInitiated && (
-        <AuthContext.Provider value={{user, setUser, handleLogout}}>
+        <AuthContext.Provider value={{ user, setUser, handleLogout }}>
           <Router>
-            <Navbar />
-
+            <Fragment>
+              <Navbar />
+              <Routes>
+                <Route path="/auth/login" element={!user ? <Login /> : <Navigate to="/" />} />
+              </Routes>
+            </Fragment>
           </Router>
         </AuthContext.Provider>
       )}
