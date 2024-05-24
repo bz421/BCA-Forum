8fc51a9d485a96024b62a22b1b27ae9934db820b
bz421 <bozhan.harry@gmail.com>
2024-05-24 16:04:30 -0400

    Emergency fix: changed server password

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoUri: 'mongodb+srv://bozhanharry:ghiloppp@forum.ekevggb.mongodb.net/?retryWrites=true&w=majority&appName=Forum'
+    mongoUri: 'mongodb+srv://bozhanharry:jap84d04U178RNX3@forum.ekevggb.mongodb.net/?retryWrites=true&w=majority&appName=Forum'
 }
