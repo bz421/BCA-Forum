cdc0c88703073242f2e17ea94dcefbff00c7e347
Kunj Parikh <9002birdwatcher@gmail.com>
2024-06-11 19:56:23 -0400

    Added Preliminary AOS

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+
 import '../home.css';
 import AuthContext from '../Contexts/AuthContext';
 import AppBar from '@material-ui/core/AppBar';
@@ -11,6 +12,8 @@ import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles';
 import { useNavigate } from 'react-router-dom';
 import {Link} from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home() {
     const { user } = useContext(AuthContext);
@@ -29,15 +32,24 @@ export default function Home() {
         handleClose()
         navigate('/auth/register')
     }
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <div style={{padding: "2rem"}}>
-            <h1>Welcome to BCA Forum!</h1>
+            <h1 data-aos ="fade-right">Welcome to BCA Forum!</h1>
             {
                 user ? 
                 (
                     <div>
                         <br />
-
+                        <div data-aos="fade-right">
+                            <h1>Check out some categories of classes.</h1>
+                            <br />
+                            <Button color="primary" variant="contained" style={{width: "100%"}}onClick={() => navigate('/category/')}>
+                                Categories
+                            </Button>
+                        </div>
                     </div>
                 )
                 :
