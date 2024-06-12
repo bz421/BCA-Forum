bf65ef267320fabb7dea42822b5e879c3e59eb47
Kunj Parikh <9002birdwatcher@gmail.com>
2024-06-11 21:38:11 -0400

    tempCommit

--- client/src/Pages/Thread/ShowThread.js
+++ client/src/Pages/Thread/ShowThread.js
@@ -157,7 +157,27 @@ export default function ShowThread() {
         <div style={{ padding: "2rem" }}>
             {thread && <h1><Latex>{thread.title + ' '}</Latex></h1>}
 
-            {(thread && (user._id === thread.userId)) && <p>You are the creator</p>}
+            {/* {(thread && (user._id === thread.userId)) && <p>You are the creator</p>} */}
+
+            {/* {(thread && (user))} */}
+            {(thread && (user._id === thread.userId)) ?
+                (
+                    <div>
+                        <p>You are the creator</p>
+                    </div>
+                )
+                :
+                (
+                    <div>
+                        
+                        <p style={{fontSize:"0.9rem"}}>By {thread.name}</p>
+                        
+                    </div>
+                    
+                    
+                )
+            }
+
 
             {thread && <p style={{ fontSize: "1.1rem" }}><Latex>{thread.content + ' '}</Latex></p>}
             <List>
